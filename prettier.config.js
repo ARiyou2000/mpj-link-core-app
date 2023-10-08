ec6a6177716dfeb0e509fcadc005eaa773f543c2
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-10-08 12:15:13 +0330

    Editor setup: Add and config prettier, eslint, and editorconfig

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Option} */
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "always",
+};
