8a0dadf52a0739df05d064255bf89e3616305053
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-10-08 15:09:09 +0330

    Force Update context: add force component update and Force Scenario Update context provider

---
+++ contexts/forceUpdateContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+import useForceUpdateUI from "../hooks/useForceUpdateUI";
+
+export const ForceUpdateContext = createContext({});
+
+const ForceUpdateProvider = ({ children }) => {
+  const refreshComponent = useForceUpdateUI;
+  return (
+    <>
+      <ForceUpdateContext.Provider value={refreshComponent}>
+        {children}
+      </ForceUpdateContext.Provider>
+    </>
+  );
+};
+
+export const ScenarioForceUpdateContext = createContext(() => null);
+
+export default ForceUpdateProvider;
