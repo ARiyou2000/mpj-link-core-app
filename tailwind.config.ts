387a8d36a5b1adf3763723b85da76f68d60cca33
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-10-08 15:14:45 +0330

    Import tailwind custom configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { fontFamily } from "tailwindcss/defaultTheme";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -16,7 +18,19 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-YekanBakh)", ...fontFamily.sans],
+        faNum: [
+          "var(--font-yekan_bakh_faNum)",
+          "var(--font-yekan_bakh)",
+          "YekanBakhTry",
+          ...fontFamily.sans,
+        ],
+      },
       colors: {
+        black: "#000000",
+        milkwhite: "#F4F4F4",
+        red: "#D04848",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -55,22 +69,95 @@ module.exports = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+        card: "1.5625rem",
+      },
+      borderWidth: {
+        "1": "0.0625rem",
+        "1.5": "0.09375rem",
+        "4": "0.25rem",
+      },
+      ringWidth: {
+        "1": "0.0625rem",
+        "1.5": "0.09375rem",
+      },
+      spacing: {
+        "1pxr": "0.0625rem",
+      },
+      dropShadow: {
+        active: [
+          "4px -4px 4px rgba(255, 255, 255, 0.20)",
+          "-4px -4px 4px rgba(255, 255, 255, 0.20)",
+          "-4px 4px 4px rgba(255, 255, 255, 0.20)",
+          "4px 4px 4px rgba(255, 255, 255, 0.20)",
+        ],
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
+        },
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+        "shine-to-left": {
+          to: {
+            "background-position-x": "200%",
+          },
+        },
+        "shine-to-right": {
+          to: {
+            "background-position-x": "-200%",
+          },
+        },
+        rotate: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        rotateBack: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "100%": {
+            transform: "rotate(-360deg)",
+          },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        wiggle: "wiggle 1s ease-in-out infinite",
+        "loading-to-left": "2.5s shine-to-left linear infinite",
+        "loading-to-right": "2.5s shine-to-right linear infinite",
+        rotate: "1s rotate linear infinite",
+        rotateBack: "1s rotateBack linear infinite",
       },
+    },
+    backgroundImage: {
+      "loading-to-left":
+        "linear-gradient(70deg, #475569FF 8%, #8291A5F0 18%, #475569FF 40%)",
+      "loading-to-right":
+        "linear-gradient(110deg, #475569FF 8%, #8291A5F0 18%, #475569FF 40%)",
+    },
+    transitionDuration: {
+      "1500": "1500ms",
+      "2000": "2000ms",
+      "2500": "2500ms",
+      "3000": "3000ms",
+    },
+    backgroundSize: {
+      loading: "200% 100%",
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
+
+export default config;
