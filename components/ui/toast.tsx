0041d9ac256fdc75e9030873d6145eddd6ec233a
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-10-11 08:59:03 +0330

    Toast: update display position

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[20rem]",
+      "p-4 fixed top-0 right-0 z-[100] flex max-h-screen w-full flex-col-reverse sm:bottom-0 sm:left-0 sm:top-auto sm:flex-col md:max-w-[20rem]",
       className,
     )}
     {...props}
