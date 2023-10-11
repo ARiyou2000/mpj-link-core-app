901f08b55c1e3440a8cc6214578244197438b839
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-10-11 11:12:05 +0330

    Icons: add Hood colored icon

--- components/icons/colored/index.js
+++ components/icons/colored/index.js
@@ -1,4 +1,5 @@
 export { default as DuctSplit } from "./DuctSplit";
+export { default as Hood } from "./Hood";
 export { default as Music } from "./Music";
 export { default as Switches } from "./Switches";
 export { default as Thermometer } from "./Thermometer";
