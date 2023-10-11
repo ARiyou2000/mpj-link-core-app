6fa3401f3979eea3a01e466e17d33cc5c9062e1f
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-10-11 12:56:29 +0330

    Gradiant border: style card with available interaction area

--- components/Card/GradiantCard/index.jsx
+++ components/Card/GradiantCard/index.jsx
@@ -1,16 +1,11 @@
 import NormalCard from "../NormalCard";
-// import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
 const GradiantCard = ({ children, className, ...props }) => {
   return (
     <>
       <NormalCard
-        // className={cn("border-gradiant", className)}
-        className={className}
-        // style={{
-        //   borderImageSource:
-        //     "linear-gradient(215.38deg, #FB9393 -6.7%, #FFFFFF 135.15%)",
-        // }}
+        className={cn("border-gradiant border-none [&>*]:z-10", className)}
         {...props}>
         {children}
       </NormalCard>
