0d78265827af348c8052c10de47792fc5b5b4d3f
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-10-30 14:24:14 +0330

    IR Split: Edit view and functionality

--- app/(dashboard)/devices/[deviceCategory]/[devicePublicId]/@irSplitDevicePage/page.jsx
+++ app/(dashboard)/devices/[deviceCategory]/[devicePublicId]/@irSplitDevicePage/page.jsx
@@ -4,17 +4,25 @@ import DeviceHeader from "@/components/deviceAndZoneHeader/DeviceHeader";
 import useDeviceData from "@/hooks/useDeviceData";
 import { setRegisterData } from "@/utils/queueHelper";
 import SplitIR from "@/components/devicePagesBody/SplitIR";
+import { useState } from "react";
 
 const IrSplitDevicePage = () => {
   const [info, deviceRegistersInfoAndData] = useDeviceData({
     hasFeedback: false,
   });
 
-  const handleDeviceUpdate = (value) => {
-    setRegisterData(deviceRegistersInfoAndData[0].publicId, value, {
-      hasFeedback: false,
-    });
+  const handleDeviceUpdate = (registerIndex, value) => {
+    setRegisterData(
+      deviceRegistersInfoAndData[registerIndex]?.publicId,
+      value,
+      {
+        hasFeedback: false,
+      },
+    );
   };
+
+  const [lastHertakiPowerActionType, setLastHertakiPowerActionType] =
+    useState(false);
 
   return (
     <>
@@ -23,6 +31,10 @@ const IrSplitDevicePage = () => {
         description={info?.description}
         hasPowerButton={true}
         powerValue={false}
+        onPowerChange={() => {
+          setLastHertakiPowerActionType((prevState) => !prevState);
+          handleDeviceUpdate(0, lastHertakiPowerActionType ? "01" : "02");
+        }}
       />
       <SplitIR
         className={"flex-1 h-0 w-full"}
--- components/devicePagesBody/SplitIR/index.jsx
+++ components/devicePagesBody/SplitIR/index.jsx
@@ -43,7 +43,7 @@ const controlButtonStyleClassName =
 
 const SplitIR = ({
   className,
-  handleDeviceUpdate = (value) => null,
+  handleDeviceUpdate = (registerIndex, value) => null,
   ...props
 }) => {
   return (
@@ -54,31 +54,55 @@ const SplitIR = ({
             className={
               "px-7 py-[4.5rem] flex flex-row items-center justify-between"
             }>
-            <Button className={controlButtonStyleClassName}>
+            <Button
+              className={controlButtonStyleClassName}
+              onClick={() => {
+                handleDeviceUpdate(2, "01");
+              }}>
               <Plus className={primaryControlIconStyleClassNameGroup2} />
             </Button>
             <div
               className={"flex flex-col justify-between items-center gap-12"}>
-              <Button className={controlButtonStyleClassName}>
+              <Button
+                className={cn(controlButtonStyleClassName, "invisible")}
+                onClick={() => {
+                  handleDeviceUpdate(99, "0000000000000");
+                }}>
                 <Up className={primaryControlIconStyleClassNameGroup1} />
               </Button>
 
-              <Button className={controlButtonStyleClassName}>
+              <Button
+                className={cn(controlButtonStyleClassName, "invisible")}
+                onClick={() => {
+                  handleDeviceUpdate(99, "0000000000000");
+                }}>
                 <Down className={primaryControlIconStyleClassNameGroup1} />
               </Button>
             </div>
-            <Button className={controlButtonStyleClassName}>
+            <Button
+              className={controlButtonStyleClassName}
+              onClick={() => {
+                handleDeviceUpdate(1, "01");
+              }}>
               <Minus className={primaryControlIconStyleClassNameGroup2} />
             </Button>
           </div>
 
-          <div className={"flex flex-row gap-6"}>
+          <div className={"flex-row gap-6 hidden"}>
             <SplitIRControlCard title={"تغییر حرکت پره ها"}>
               <div className={"flex flex-row justify-between items-center"}>
-                <Button className={controlButtonStyleClassName}>
+                <Button
+                  className={controlButtonStyleClassName}
+                  onClick={() => {
+                    handleDeviceUpdate("000000000000");
+                  }}>
                   <LeftRight className={secondaryControlIconStyleClassName} />
                 </Button>
-                <Button className={controlButtonStyleClassName}>
+                <Button
+                  className={controlButtonStyleClassName}
+                  onClick={() => {
+                    handleDeviceUpdate("000000000000");
+                  }}>
                   <UpDown className={secondaryControlIconStyleClassName} />
                 </Button>
               </div>
@@ -86,7 +110,11 @@ const SplitIR = ({
 
             <SplitIRControlCard title={"تغییر حالت"}>
               <div className={"flex items-center justify-center"}>
-                <Button className={controlButtonStyleClassName}>
+                <Button
+                  className={controlButtonStyleClassName}
+                  onClick={() => {
+                    handleDeviceUpdate("000000000000");
+                  }}>
                   <Breeze className={secondaryControlIconStyleClassName} />
                 </Button>
               </div>
