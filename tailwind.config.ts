92d017734e90058bf51dbffebc32a76a48716562
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-10-31 10:47:22 +0330

    Global Style: Rearrange style order and use tailwind directives

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
+    "./hooks/**/*.{js,jsx,ts,tsx}",
     "./app/**/*.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
