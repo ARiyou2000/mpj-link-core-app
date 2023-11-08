6c5c3032378e33e8c83a365ac51bdb2f231b0640
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-11-08 18:15:06 +0330

    Create zigbee curtains class

--- classes/registers/modbus/curtainsRegisters.ts
+++
@@ -1,7 +0,0 @@
-import GeneralPower from "@/classes/registers/modbus/generalPower";
-
-class CurtainsRegister extends GeneralPower {}
-
-export class CurtainsOpen extends CurtainsRegister {}
-
-export class CurtainsClose extends CurtainsRegister {}
