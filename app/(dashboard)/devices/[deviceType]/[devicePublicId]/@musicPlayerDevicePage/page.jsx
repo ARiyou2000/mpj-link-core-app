3c0aa65e8c4848c12be0191f1d2c112627f9928a
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-11-12 16:18:53 +0330

    Music player: handle on/off register update

--- app/(dashboard)/devices/[deviceType]/[devicePublicId]/@musicPlayerDevicePage/page.jsx
+++ app/(dashboard)/devices/[deviceType]/[devicePublicId]/@musicPlayerDevicePage/page.jsx
@@ -3,9 +3,23 @@
 import DeviceHeader from "@/components/deviceAndZoneHeader/DeviceHeader";
 import useDeviceData from "@/hooks/useDeviceData";
 import MusicPlayer from "@/components/devicePagesBody/MusicPlayer";
+import { useState } from "react";
+import useRegisterUpdateToast from "../../../../../../hooks/useRegisterUpdateToast";
 
 const MusicPlayerDevicePage = () => {
   const device = useDeviceData();
+
+  const [hertakiUpdateFlag, setHertakiUpdateFlag] = useState(false);
+  const [registerUpdateHandler] = useRegisterUpdateToast();
+
+  const handelHertakiUpdate = async (fistAction, secoundAction) => {
+    setHertakiUpdateFlag((prevState) => !prevState);
+    if (hertakiUpdateFlag) {
+      await registerUpdateHandler(fistAction);
+    } else {
+      await registerUpdateHandler(secoundAction);
+    }
+  };
 
   return (
     <>
@@ -15,10 +29,16 @@ const MusicPlayerDevicePage = () => {
           description={device?.description}
           hasPowerButton={true}
           powerValue={false}
-          onPowerChange={device?.powerOn}
+          onPowerChange={() => {
+            handelHertakiUpdate(device?.powerOn, device?.powerOff);
+          }}
         />
       </div>
-      <MusicPlayer className={"flex-1 h-0 w-full"} deviceInstance={device} />
+      <MusicPlayer
+        className={"flex-1 h-0 w-full"}
+        deviceInstance={device}
+        updateHandler={handelHertakiUpdate}
+      />
     </>
   );
 };
