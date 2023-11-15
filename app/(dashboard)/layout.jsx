3abe25fbd4d4b00491b10430c7277f35e7bd7b90
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-11-15 17:07:12 +0330

    Navigation menu: move navigation menu to right side on landscape orientation

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -12,9 +12,12 @@ const MainLayout = ({ children, className, ...props }) => {
   return (
     <>
       <div
-        className={cn("h-full w-full flex flex-col justify-between", className)}
+        className={cn(
+          "h-full w-full flex flex-col landscape:flex-row justify-between",
+          className,
+        )}
         {...props}>
-        <MainLayoutHeader />
+        <MainLayoutHeader className={"landscape:hidden"} />
         <div className={"flex-1 h-0 pt-3"}>{children}</div>
         <MainLayoutTabNavigation />
       </div>
--- components/MainLayoutTabNavigation/index.jsx
+++ components/MainLayoutTabNavigation/index.jsx
@@ -7,7 +7,7 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { motion } from "framer-motion";
 
-const iconsClassName = "h-6 w-6 mx-auto";
+const iconsClassName = "h-6 w-6";
 const NavBarItems = [
   {
     name: "اینترنت",
@@ -64,29 +64,33 @@ const MainLayoutTabNavigation = () => {
   }, [pathname]);
 
   return (
-    <div className="bg-[#383838] max-h-[4.4rem] px-6 rounded-t-xl overflow-hidden">
-      <ul className="flex flex-row justify-evenly">
+    <div className="bg-[#383838] w-full landscape:h-full landscape:w-auto px-6 landscape:px-6 rounded-t-xl landscape:rounded-r-xl landscape:rounded-tl-none overflow-hidden">
+      <ul className="flex flex-row landscape:flex-col landscape:h-full justify-evenly items-center">
         {NavBarItems.map((menuItem) => (
-          <Link href={menuItem.link} key={`NavBarItem_${menuItem.link}`}>
-            <li className="flex flex-col text-center pt-6 w-[4.275rem] relative transition">
+          <Link
+            className={"flex-1"}
+            href={menuItem.link}
+            key={`NavBarItem_${menuItem.link}`}>
+            <li className="w-full h-full flex flex-col items-center justify-center text-center pt-6 landscape:pt-0 landscape:w-auto relative transition">
               {activeMenuIndex === menuItem.link && (
                 <motion.span
                   layoutId="bubble"
-                  className={`bg-[#1C1F20] h-16 w-[4.275rem] absolute -top-8 rounded-full bg-clip-content`}
+                  // style={{ rotate: 90 }}
+                  className={`bg-[#1C1F20] h-16 w-16 rounded-full my-auto mx-auto absolute -top-8 landscape:top-auto landscape:-right-14`}
                   transition={{ type: "spring", bounce: 0.5, duration: 0.6 }}>
-                  <span className="w-3.5 h-3.5 bg-transparent absolute top-8 -left-[0.78rem] rounded-tr-[0.6875rem] shadow-[0.25625rem_-0.3125rem_0_0_#1C1F20]"></span>
-                  <span className="w-3.5 h-3.5 bg-transparent absolute top-8 -right-[0.78rem] rounded-tl-[0.6875rem] shadow-[-0.25625rem_-0.3125rem_0_0_#1C1F20]"></span>
+                  <span className="w-3.5 h-3.5 bg-transparent absolute top-8 landscape:-top-3 -left-[0.78rem] landscape:left-auto landscape:rotate-90 rounded-tr-[0.6875rem] shadow-[0.25625rem_-0.3125rem_0_0_#1C1F20]"></span>
+                  <span className="w-3.5 h-3.5 bg-transparent absolute top-8 landscape:top-auto landscape:-bottom-3 -right-[0.78rem] landscape:right-auto landscape:rotate-90 rounded-tl-[0.6875rem] shadow-[-0.25625rem_-0.3125rem_0_0_#1C1F20]"></span>
                 </motion.span>
               )}
               <span
-                className={`text-xl cursor-pointer duration-300 ${
+                className={`inline-flex items-center justify-center text-xl cursor-pointer duration-300 ${
                   activeMenuIndex === menuItem.link &&
-                  "-translate-y-6 text-white"
+                  "-translate-y-6 text-white landscape:translate-y-0 landscape:translate-x-5"
                 }`}>
                 {menuItem.icon}
               </span>
               <span
-                className={`duration-300 ${
+                className={`duration-300 landscape:hidden ${
                   activeMenuIndex === menuItem.link
                     ? "opacity-100 -translate-y-2"
                     : "opacity-0 translate-y-10 "
