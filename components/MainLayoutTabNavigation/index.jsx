3d45278f4b5c2623315cab0c7f4278aa34618f24
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-11-15 12:15:11 +0330

    Main layout tab navigation: update logic and add animation

--- components/MainLayoutTabNavigation/index.jsx
+++ components/MainLayoutTabNavigation/index.jsx
@@ -5,37 +5,33 @@ import { Cpu, Grip, Home, SettingAlt } from "@/components/icons/dashed";
 import { Cloud } from "@/components/icons";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const iconsClassName = "h-6 w-6 mx-auto";
 const NavBarItems = [
   {
     name: "اینترنت",
     icon: <Cloud className={iconsClassName} />,
-    dis: "translate-x-0",
     link: "/internet",
   },
   {
     name: "کور",
     icon: <Cpu className={iconsClassName} />,
-    dis: "-translate-x-[4.275rem]",
     link: "/core",
   },
   {
     name: "خانه",
     icon: <Home className={iconsClassName} />,
-    dis: "-translate-x-[8.55rem]",
     link: "/home",
   },
   {
     name: "تنظیمات",
     icon: <SettingAlt className={iconsClassName} />,
-    dis: "-translate-x-[12.825rem]",
     link: "/setting",
   },
   {
     name: "دستگاه‌ها",
     icon: <Grip className={iconsClassName} />,
-    dis: "-translate-x-[17.1rem]",
     link: "/devices",
   },
 ];
@@ -43,17 +39,17 @@ const NavBarItems = [
 const getActiveIndex = (pathname) => {
   if (pathname) {
     if (pathname.includes("devices")) {
-      return 4;
+      return "/devices";
     } else if (pathname.includes("setting")) {
-      return 3;
+      return "/setting";
     } else if (pathname.includes("home")) {
-      return 2;
+      return "/home";
     } else if (pathname.includes("core")) {
-      return 1;
+      return "/core";
     } else if (pathname.includes("internet")) {
-      return 0;
+      return "/internet";
     } else {
-      return 2;
+      return "/home";
     }
   }
 };
@@ -69,35 +65,36 @@ const MainLayoutTabNavigation = () => {
 
   return (
     <div className="bg-[#383838] max-h-[4.4rem] px-6 rounded-t-xl overflow-hidden">
-      <ul className="flex flex-row relative ">
-        <span
-          className={`bg-[#1C1F20] duration-500 ${NavBarItems[activeMenuIndex].dis} h-16 w-[4.275rem] absolute -top-8 rounded-full`}>
-          <span className="w-3.5 h-3.5 bg-transparent absolute top-8 -left-[0.78rem] rounded-tr-[0.6875rem] shadow-[0.25625rem_-0.3125rem_0_0_#1C1F20]"></span>
-          <span className="w-3.5 h-3.5 bg-transparent absolute top-8 -right-[0.78rem] rounded-tl-[0.6875rem] shadow-[-0.25625rem_-0.3125rem_0_0_#1C1F20]"></span>
-        </span>
-        {NavBarItems.map((menuItem, index) => (
-          <li key={`NavBarItem_${index}`} className="w-[4.275rem]">
-            <Link
-              className="flex flex-col text-center pt-6"
-              href={menuItem.link}
-              // onClick={() => setActive(i)}
-            >
+      <ul className="flex flex-row justify-evenly">
+        {NavBarItems.map((menuItem) => (
+          <Link href={menuItem.link} key={`NavBarItem_${menuItem.link}`}>
+            <li className="flex flex-col text-center pt-6 w-[4.275rem] relative transition">
+              {activeMenuIndex === menuItem.link && (
+                <motion.span
+                  layoutId="bubble"
+                  className={`bg-[#1C1F20] h-16 w-[4.275rem] absolute -top-8 rounded-full bg-clip-content`}
+                  transition={{ type: "spring", bounce: 0.5, duration: 0.6 }}>
+                  <span className="w-3.5 h-3.5 bg-transparent absolute top-8 -left-[0.78rem] rounded-tr-[0.6875rem] shadow-[0.25625rem_-0.3125rem_0_0_#1C1F20]"></span>
+                  <span className="w-3.5 h-3.5 bg-transparent absolute top-8 -right-[0.78rem] rounded-tl-[0.6875rem] shadow-[-0.25625rem_-0.3125rem_0_0_#1C1F20]"></span>
+                </motion.span>
+              )}
               <span
-                className={`text-xl cursor-pointer duration-500 ${
-                  activeMenuIndex === index && "-mt-6 text-white translate-x-0"
+                className={`text-xl cursor-pointer duration-300 ${
+                  activeMenuIndex === menuItem.link &&
+                  "-translate-y-6 text-white"
                 }`}>
                 {menuItem.icon}
               </span>
               <span
-                className={`${
-                  activeMenuIndex === index
-                    ? "translate-y-4 duration-700 opacity-100"
-                    : "opacity-0 translate-y-10"
+                className={`duration-300 ${
+                  activeMenuIndex === menuItem.link
+                    ? "opacity-100 -translate-y-2"
+                    : "opacity-0 translate-y-10 "
                 }`}>
                 {menuItem.name}
               </span>
-            </Link>
-          </li>
+            </li>
+          </Link>
         ))}
       </ul>
     </div>
