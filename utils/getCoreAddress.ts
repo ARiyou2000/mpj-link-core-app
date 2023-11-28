a34b49e4b05cbe7e80fcec0e6a1d6a549a2987d5
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-11-28 17:37:54 +0330

    Create method to get backend address for serverside

---
+++ utils/getCoreAddress.ts
@@ -0,0 +1,7 @@
+const getCoreAddress = (): string => {
+  const ipString = process.env.NEXT_PUBLIC_ABSOLUTE_URL || "localhost";
+  return `http://${ipString}:3456/mlcore/v3`;
+};
+
+export const coreAdress = getCoreAddress();
+export default getCoreAddress;
