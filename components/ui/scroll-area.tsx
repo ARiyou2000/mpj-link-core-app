63502c16e6647d49c4d1e03b8eff584cee6f48f9
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-11-28 09:06:51 +0330

    Scroll Area: add variants for horizontal and vertical view port

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -4,16 +4,29 @@ import * as React from "react";
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
 
 import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const scrollAreaViewportVariants = cva("h-full w-full rounded-[inherit]", {
+  variants: {
+    variant: {
+      horizontal: "py-3",
+      vertical: "px-3",
+    },
+  },
+  defaultVariants: { variant: "vertical" },
+});
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> &
+    VariantProps<typeof scrollAreaViewportVariants>
+>(({ className, children, variant, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
     className={cn("relative overflow-hidden", className)}
     {...props}>
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit] px-3">
+    <ScrollAreaPrimitive.Viewport
+      className={scrollAreaViewportVariants({ variant })}>
       {children}
     </ScrollAreaPrimitive.Viewport>
     <ScrollBar />
