5c2407d943a9c642ad03b9bfc768ef687d4809b4
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-11-30 11:20:27 +0330

    Next Auth options: update user validation logic

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -3,81 +3,46 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { coreAdress } from "@/utils/getCoreAddress";
 
 export const authOptions: AuthOptions = {
+  pages: { signIn: "/" },
   providers: [
     CredentialsProvider({
       name: "Credectials",
       credentials: {
         // username: { label: "Username", type: "text", value: "user" },
-        password: { label: "Password", type: "password" },
+        passcode: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
         const base64data = Buffer.from(
-          `user:${credentials?.password}`,
+          `user:${credentials?.passcode}`,
         ).toString("base64");
         try {
-          console.log(base64data);
-
           const authResponse = await fetch(`${coreAdress}/login`, {
             headers: {
               "Content-Type": "application/json",
               Authorization: `Basic ${base64data}`,
             },
           });
+          // await fetch("/api/login", {
+          //   method: "POST",
+          //   body: JSON.stringify({ passcode: credentials?.passcode  }),
+          // });
 
           if (!authResponse.ok) {
             console.log("NOT OK");
             return null;
           }
-          // await loginWithCode(credentials?.password as string);
-          // const user = await authResponse.json();
-          const user = { name: "jimsa", publicId: "kalsj-oweru-fjklg" };
+
+          const result = await authResponse.json();
+
+          const user = { name: "admin", publicId: "admin" };
           console.log(user);
 
           return user;
         } catch (e) {
+          console.error(e);
           return null;
         }
       },
     }),
   ],
-  // pages: { signIn: "/" },
 } satisfies AuthOptions;
-
-/*
-import { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-
-export const authOptions: AuthOptions = {
-  // callbacks: {
-  //   jwt: (params) => {},
-  // },
-  providers: [
-    CredentialsProvider({
-      name: "Credectials",
-      credentials: {
-        // username: { label: "Username", type: "text", value: "user" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        try {
-          const authResponse = await fetch("/api/login", {
-            method: "POST",
-            body: JSON.stringify({ passcode: credentials?.password }),
-          });
-
-          // await loginWithCode(credentials?.password as string);
-          // const user = await authResponse.json();
-          const user = { name: "jimsa", publicId: "kalsj-oweru-fjklg" };
-          console.log(user);
-
-          return user;
-        } catch (e) {
-          return null;
-        }
-      },
-    }),
-  ],
-  // pages: { signIn: "/" },
-} satisfies AuthOptions;
-
-*/
