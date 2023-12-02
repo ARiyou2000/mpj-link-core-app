731a4ad1474cdc6bde5897b0c6c2d7128ba5826c
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-12-02 16:42:02 +0330

    Hope page: fetch data in server side

--- app/(dashboard)/home/page.jsx
+++ app/(dashboard)/home/page.jsx
@@ -1,16 +1,10 @@
-"use client";
-
 import ZonesList from "@/components/listCardPairs/zones/ZonesList";
 import ScenariosList from "@/components/listCardPairs/scenarios/ScenariosList";
-import { getScenarios, getZones } from "@/utils/getStaticData";
-import useStaticData from "@/hooks/useStaticData";
-import { ScenarioForceUpdateContext } from "@/contexts/forceUpdateContext";
-import Scenario from "@/classes/scenario";
-import Zone from "@/classes/zone";
+import authorizedFetch from "@/utils/authorizedFetch";
 
-const HomePage = () => {
-  const [zonesData] = useStaticData(getZones);
-  const [scenarioData, scenarioForceUpdate] = useStaticData(getScenarios);
+const HomePage = async () => {
+  const zonesData = await authorizedFetch("zone");
+  const scenarioData = await authorizedFetch("scenario");
 
   return (
     <>
@@ -20,28 +14,15 @@ const HomePage = () => {
         }>
         <div className={"w-full flex flex-col items-start"}>
           <h3 className={"px-4"}>زون ها</h3>
-          <ZonesList
-            list={zonesData?.map(
-              ({ publicId, name, description, image }, index) =>
-                new Zone(publicId, name, description || "", image || ""),
-            )}
-          />
+          <ZonesList list={zonesData} />
         </div>
 
         <div className={"flex-1 h-0 px-1 flex flex-col gap-5 items-start"}>
           <h3 className={"px-3"}>سناریو ها</h3>
-          <ScenarioForceUpdateContext.Provider value={scenarioForceUpdate}>
-            <ScenariosList
-              list={scenarioData?.map(
-                ({ publicId, name, description, image, favorite }, index) =>
-                  new Scenario(publicId, name, description, image, favorite),
-              )}
-            />
-          </ScenarioForceUpdateContext.Provider>
+          <ScenariosList list={scenarioData} />
         </div>
       </div>
     </>
   );
 };
-
 export default HomePage;
