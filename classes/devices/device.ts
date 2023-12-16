1d2860efd7e3fa7952212b9556506d9314642066
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-12-16 18:07:35 +0330

    Device class: Implement get device data method for modbus and zigbee devices

--- classes/devices/device.ts
+++ classes/devices/device.ts
@@ -1,15 +1,15 @@
 import Register, { objectType } from "@/classes/registers/register";
 import DeviceInfo from "@/classes/devices/deviceInfo";
 import { Protocols } from "@/classes/protocols";
+import clientSideAuthorizedFetch from "@/utils/clientSideAuthorizedFetch";
+import { getRegistersValueFormString } from "@/hooks/useDeviceData";
+import { getDataOptionsType } from "@/utils/queueHelper";
+import { messageType } from "@/mqtt";
 
 export type RegistersListObjectType = { [key: string]: Register };
 
 class Device extends DeviceInfo {
   private _registers: RegistersListObjectType;
-  // @ts-ignore
-  #protocol: Protocols;
-  // @ts-ignore
-  #hasDataFeedback: boolean;
 
   constructor(
     protocol: Protocols,
@@ -18,39 +18,53 @@ class Device extends DeviceInfo {
     description: string,
     type: number,
     registers: RegistersListObjectType,
-    hasDataFeedback: boolean,
   ) {
     super(publicId, name, description, type);
-    this.#protocol = protocol;
     this._registers = registers;
-    this.#hasDataFeedback = hasDataFeedback;
   }
 
   get registers(): RegistersListObjectType {
     return this._registers;
   }
 
-  get protocol(): Protocols {
-    return this.#protocol;
-  }
-
-  get hasDataFeedback(): boolean {
-    return this.#hasDataFeedback;
-  }
-
   valueAssignment(values: string[] | objectType) {}
 
-  // // @ts-ignore
-  // #getData = (signal: AbortSignal) => {
-  //   if (this.#protocol === Protocols.modbus) {
-  //   } else if (this.#protocol === Protocols.zigbee) {
-  //   } else {
-  //   }
-  // };
-  //
-  // get getData(): (signal: AbortSignal) => void {
-  //   return this.#getData;
-  // }
+  // @ts-ignore
+  #getData = async (options: getDataOptionsType, zigbeeData?: messageType) => {
+    if (this.hasDataFeedback) {
+      if (this.protocol === Protocols.modbus) {
+        // Device registers current value form server
+        const registersStringValue = (await clientSideAuthorizedFetch(
+          `/api/devices/${this.publicId}/data`,
+          options,
+        )) as string;
+
+        // Get registers value from string
+        if (!registersStringValue || !Number(registersStringValue)) {
+          throw new Error("Registers value is null");
+        }
+
+        const registersValueArray =
+          getRegistersValueFormString(registersStringValue);
+
+        this.valueAssignment(registersValueArray);
+      } else if (this.protocol === Protocols.zigbee) {
+        if (!!zigbeeData) {
+          const dataObject = JSON.parse(zigbeeData.toString());
+          this.valueAssignment(dataObject);
+        }
+      } else {
+        throw new Error("Unsupported protocol");
+      }
+    }
+  };
+
+  get getData(): (
+    options: getDataOptionsType,
+    zigbeeData?: messageType,
+  ) => Promise<void> {
+    return this.#getData;
+  }
 }
 
 export default Device;
