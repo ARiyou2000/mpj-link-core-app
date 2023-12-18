cc029809e93bf5b0942864b4622731a780b9b42b
ARiyou2000 <AR.Jahan2000@gmail.com>
2023-12-18 18:32:06 +0330

    Registers: add template to read the value of a register directly

--- app/api/devices/[api_devicePublicId]/registers/[api_deviceRegisterPublicId]/data/route.ts
+++ app/api/devices/[api_devicePublicId]/registers/[api_deviceRegisterPublicId]/data/route.ts
@@ -1,11 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import ApiResponse from "@/app/api/apiResponse";
 import { Protocols } from "@/classes/protocols";
-import { setRegisterData } from "@/utils/queueHelper";
 import { paramsType } from "@/app/api/devices/[api_devicePublicId]/registers/[api_deviceRegisterPublicId]/route";
 import mqttPublish from "@/mqtt/publish";
 import connectionConfig from "@/connection.config";
 import getAuthorizationHeaders from "@/utils/getAuthorizationHeaders";
+import { setRegisterData } from "@/utils/queueHelper";
+// import fetchUrl from "@/utils/fetchUrl";
+// import DeviceInfo, {
+//   ServerSideDeviceInfoT,
+// } from "@/classes/devices/deviceInfo";
 
 export const PUT = async (
   request: NextRequest,
@@ -37,3 +41,44 @@ export const PUT = async (
 
   return NextResponse.json(new ApiResponse(true, result || {}));
 };
+
+// export const GET = async (
+//   request: NextRequest,
+//   { params: { api_devicePublicId, api_deviceRegisterPublicId } }: paramsType,
+// ) => {
+//   const body = await request.json();
+//   const signal = request.signal;
+//
+//   const { value, hasFeedback, protocol, indicator } = body;
+//   const options = { signal, headers: getAuthorizationHeaders(request.headers) };
+//
+//   const device = (await fetchUrl(
+//     `${process.env.NEXT_SELF_ABSOLUTE_URL}/api/devices/${api_devicePublicId}`,
+//     options,
+//   )) as ServerSideDeviceInfoT;
+//
+//   const deviceInfo = new DeviceInfo(
+//     device.publicId,
+//     device.name,
+//     device.description,
+//     device.type,
+//   );
+//
+//   if (deviceInfo.protocol === Protocols.modbus) {
+//     const deviceRegistersValue = await getRegisterData(
+//       api_deviceRegisterPublicId,
+//       options,
+//     );
+//
+//     return NextResponse.json(new ApiResponse(true, deviceRegistersValue.value));
+//   } else if (deviceInfo.protocol === Protocols.zigbee) {
+//     await mqttPublish({
+//       topic: `${connectionConfig.mqtt.mainTopic}/${api_devicePublicId}/get`,
+//       message: JSON.stringify({ [indicator]: "" }),
+//     });
+//
+//     return NextResponse.json({});
+//   } else {
+//     return NextResponse.error();
+//   }
+// };
